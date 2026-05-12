b630264327bcd295d53072ae65bd364b572030ab
tsvalencio-IA <tsvalencio@gmail.com>
2026-05-11 23:10:27 -0300

    Add files via upload

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'controle-fiados-offline-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+const ASSETS = ['index.html', 'manifest.webmanifest', 'sw.js'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
@@ -8,5 +8,5 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html'))));
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('index.html'))));
 });
